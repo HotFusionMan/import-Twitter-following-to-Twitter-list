22b8bbf00a291d333623f0283b060eb02f3cb75f
Albert Davidson Chou <HotFusionMan@Yahoo.com>
2010-01-30 15:02:40 -0800

    Comments on how the list name became what it is.  Added progress indicator.

--- twitter_following_to_list.rb
+++ twitter_following_to_list.rb
@@ -18,18 +18,22 @@ httpauth = Twitter::HTTPAuth.new( twitter_username, twitter_password, :ssl => TR
 base = Twitter::Base.new( httpauth )
 
 
-list_name = 'my-following'
+list_name = 'my-following' # "following" is not an allowed name for a Twitter list.
+# Underscores in Twitter list names are silently replaced with hyphens at list creation time!
+
 list_options = { :name => list_name }
-#list_options[:mode] =  if
-#list_options[:description] =  if
+#list_options[:mode] =  if # public/private mode was specified
+#list_options[:description] =  if # a description of the list was provided
 list = base.list_create( twitter_username, list_options ) #unless list exists already, in which case skip creation and add only the users who aren't already in the list
 
 
 friend_ids = base.friend_ids
 # TODO:  Comply with Twitter's new cursoring strategy:
 # number_of_friend_id_pages =
+number_of_friends = friend_ids.length
+friend_ids.reverse.each_with_index { |i, id|
+  puts "Adding #{i} of #{number_of_friends}"
 
-friend_ids.reverse.each { |id|
   begin
     base.list_add_member( twitter_username, list_name, id )
   rescue Twitter::NotFound => e
